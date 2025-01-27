cc0e7348d79a1c8dc51246909511fd9ae5afadd9
Rafi Adi Pramana <rafiadipramana@gmail.com>
2025-01-27 17:54:39 +0700

    docs(all): add all completed codes

--- index.js
+++ index.js
@@ -24,12 +24,33 @@ import { join } from 'path';
 import { readFileSync } from 'fs';
 
 /// Create an express server instance
+const app = express();
 
 /// '/' route to check if the server is running
+app.get('/', (_, res) => {
+    res.send('Deep Link Web Server is running');
+});
 
 /// '/.well-known/assetlinks.json' route to serve the assetlinks.json file
+app.get('/.well-known/assetlinks.json', (_, res) => {
+    try {
+        const filePath = join(process.cwd(), 'assetlinks.json');
+        const data = readFileSync(filePath, 'utf8');
+        
+        res.setHeader('Content-Type', 'application/json');
+        return res.status(200).send(data);
+    } catch (err) {
+        console.error('Error reading assetlinks.json:', err);
+        return res.status(500).json({ 
+            error: 'Failed to read assetlinks.json file' 
+        });
+    }
+});
 
 /// Start the server
+app.listen(4000, () => {
+    console.log(`Server is running on port 4000`);
+});
 
 // TODO 4: Prepare vercel.json file
 /// Complete the vercel.json file with the following content:
